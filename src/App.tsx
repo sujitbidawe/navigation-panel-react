746defae8e20f43c4f28303e908fce5f81e62039
sujitbidawe <bidawesujit@gmail.com>
2022-11-07 23:37:33 +0530

    REFACTORED: code to generate dynamic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import './App.css';
 import { _getNavs } from './utils/_DATA.js';
 import { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Sidebar from './pages/sidebar/Sidebar';
 import Breadcrumbs from './pages/breadcrumbs/Breadcrumbs';
 import * as React from 'react';
-const Login = React.lazy(() => import('./pages/login/Login'));
-const Dashboard = React.lazy(() => import('./pages/dashboard/Dashboard'));
-const Analysis = React.lazy(() => import('./pages/dashboard/analysis/Analysis'));
-const Monitor = React.lazy(() => import('./pages/dashboard/monitor/Monitor'));
-const Workplace = React.lazy(() => import('./pages/dashboard/workplace/Workplace'));
-const List = React.lazy(() => import('./pages/projects/list/List'));
-const Details = React.lazy(() => import('./pages/projects/details/Details'));
-const Settings = React.lazy(() => import('./pages/projects/settings/Settings'));
-const NotFound = React.lazy(() => import('./pages/pageNotFound/PageNotFound'));
 
 function App() {
 
@@ -49,95 +40,42 @@ function App() {
 		navigate(nav.path);
 	}
 
+	function flattenRoutes(navs: any): any {
+		return Object.keys(navs).map((route: any) => [navs[route].routes ? flattenRoutes(navs[route].routes) : [], navs[route]]).flat(Infinity);
+	}
+
 	return (
 		<div className="App">
 			<Breadcrumbs navs={navs}></Breadcrumbs>
 			<div className='container'>
 				<Sidebar navs={navs} mouseEnter={mouseEnter} mouseLeave={mouseLeave} routeClicked={routeClicked} />
 				<div className='right-container'>
-				<Routes>
-					<Route
-						path='/'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<h1>Landing page</h1>
-							</React.Suspense>
+					<Routes>
+						<Route
+							path='/'
+							element={
+								<React.Suspense fallback={<>Loading...</>}>
+									<h1>Landing page</h1>
+								</React.Suspense>
+							}
+						/>
+						{
+							flattenRoutes(navs).map((nav: any, index: number) => {
+								return (
+									<Route
+										key={index}
+										path={nav.path}
+										element={
+											<React.Suspense fallback={<>Loading...</>}>
+												<nav.compName />
+											</React.Suspense>
+										}
+									/>
+								)
+
+							})
 						}
-					/>
-					<Route
-						path='/user/login'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Login />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/dashboard'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Dashboard />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/dashboard/analysis'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Analysis />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/dashboard/monitor'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Monitor />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/dashboard/workplace'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Workplace />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/projects'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Navigate to={'/projects/list'} replace />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/projects/list'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<List />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/projects/:id'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Details />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/projects/:id/settings'
-						element={
-							<React.Suspense fallback={<>Loading...</>}>
-								<Settings />
-							</React.Suspense>
-						}
-					/>
-					<Route path="*" element={<NotFound />} />
-				</Routes>
+					</Routes>
 				</div>
 			</div>
 		</div>
--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,8 +1,20 @@
+import * as React from 'react';
+const Login = React.lazy(() => import('../pages/login/Login'));
+const Dashboard = React.lazy(() => import('../pages/dashboard/Dashboard'));
+const Analysis = React.lazy(() => import('../pages/dashboard/analysis/Analysis'));
+const Monitor = React.lazy(() => import('../pages/dashboard/monitor/Monitor'));
+const Workplace = React.lazy(() => import('../pages/dashboard/workplace/Workplace'));
+const List = React.lazy(() => import('../pages/projects/list/List'));
+const Details = React.lazy(() => import('../pages/projects/details/Details'));
+const Settings = React.lazy(() => import('../pages/projects/settings/Settings'));
+const Notfound = React.lazy(() => import('../pages/pageNotFound/PageNotFound'));
+
 let navs = [
     {
         name: 'Login',
+        compName: Login,
         locale: 'user.login',
-        path: '/user/login',
+        path: '/login',
         component: 'pages/login',
         hideInMenu: true,
     },
@@ -10,6 +22,7 @@ let navs = [
         path: '/dashboard',
         locale: 'dashboard',
         name: 'Dashboard',
+        compName: Dashboard,
         icon: 'dashboard',
         exact: true, // This route will only work for /dashboard . For /dashboard/analysis or other this component will not be rendered
         component: 'pages/dashboard',
@@ -18,6 +31,7 @@ let navs = [
                 path: '/dashboard/analysis',
                 locale: 'dashboard.analysis',
                 name: 'analysis',
+                compName: Analysis,
                 component: 'pages/dashboard/analysis',
                 exact: true,
                 accessTO: ['admin'] // Allow only admins to view this menu and access this page
@@ -27,6 +41,7 @@ let navs = [
                 locale: 'dashboard.monitor',
                 component: 'pages/dashboard/monitor',
                 name: 'monitor',
+                compName: Monitor,
                 exact: true,
             },
             {
@@ -34,6 +49,7 @@ let navs = [
                 locale: 'dashboard.workplace',
                 component: 'pages/dashboard/workplace',
                 name: 'workplace',
+                compName: Workplace,
                 exact: true,
             },
         ],
@@ -42,6 +58,7 @@ let navs = [
         path: '/projects',
         locale: 'projects',
         name: 'Projects',
+        compName: List,
         icon: 'projects',
         redirect: '/projects/list', //Redirect /projects to /projects/list
         routes: [
@@ -49,6 +66,7 @@ let navs = [
                 path: '/projects/list',
                 locale: 'projects.list',
                 name: 'Projects',
+                compName: List,
                 component: 'pages/projects/list',
                 icon: 'projects',
                 exact: true,
@@ -57,6 +75,7 @@ let navs = [
                 path: '/projects/:id',
                 locale: 'projects.details',
                 name: 'Project Details',
+                compName: Details,
                 component: 'pages/projects/details',
                 icon: 'details',
                 key: 'details',
@@ -67,6 +86,7 @@ let navs = [
                 locale: 'projects.settings',
                 icon: 'settings',
                 name: 'Settings',
+                compName: Settings,
                 component: 'pages/projects/details',
                 parentKey: 'details',
                 exact: true,
@@ -75,6 +95,7 @@ let navs = [
     },
     {
         path: '*',
+        compName: Notfound,
         component: 'pages/pageNotFound',
     },
 ]
